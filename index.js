e252cfb1fa6f44c7a5505424556c53f35d73ad14
Hughes Mugera <bwosihughes24@gmail.com>
2021-02-17 01:47:52 +0300

    initial commit ,will add all website files

---
+++ index.js
@@ -0,0 +1,11 @@
+let test = ()=>{ 
+  let firstName= prompt("What is your first name");
+   let middleName = prompt("What is your middle name");
+    let lastName=prompt("What is your last name") ;
+    return alert(`Your name is ${firstName} ${middleName} ${lastName}`)
+  }
+
+  test();
+
+
+
